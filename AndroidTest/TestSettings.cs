01376dd6145e7e880196d9579d469081c0a99265
Yauheni Sheima <Yauheni_Sheima@epam.com>
2017-12-07 14:41:06 +0300

    Add project files.

---
+++ AndroidTest/TestSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidTest
+{
+    public class TestSettings
+    {
+        public string ApkPath { get; set; }
+
+        /// <summary>
+        ///     Gets variable value from App.config by its key name
+        /// </summary>
+        /// <param name="key">key name</param>
+        /// <returns>value associated with key</returns>
+        private string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public TestSettings()
+        {
+            ApkPath = Get("apkPath");
+        }
+    }
+
+}
---
+++ AndroidTest/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace AndroidTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+        TestSettings TestSettings => new TestSettings();
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the iOS app being tested is included in the solution then 
+            // add a reference to the android project from the project containing this file
+            app = ConfigureApp
+                .Android
+                // TODO: Update this path to point to your Android app and uncomment the
+                // code if the app is not included in the solution.
+                .ApkFile (TestSettings.ApkPath)
+                .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+//            app.Screenshot("First screen.");
+            app
+                .Repl();
+        }
+    }
+}
