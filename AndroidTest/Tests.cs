acf183dcbd2b7a9428ff3eb993261d723241872d
Yauheni Sheima <Yauheni_Sheima@epam.com>
2017-12-07 16:51:21 +0300

    First test

--- AndroidTest/Tests.cs
+++ AndroidTest/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -23,16 +24,61 @@ namespace AndroidTest
                 .Android
                 // TODO: Update this path to point to your Android app and uncomment the
                 // code if the app is not included in the solution.
-                .ApkFile (TestSettings.ApkPath)
+                .ApkFile(TestSettings.ApkPath)
                 .StartApp();
+        }
+
+        public void Steps()
+        {
+            //x5
+            app.Tap(c => c.Id("nextView"));
+            app
+                .Repl()
+                ;
         }
 
         [Test]
         public void AppLaunches()
         {
 //            app.Screenshot("First screen.");
-            app
-                .Repl();
+            for (int i = 0; i < 5; i++)
+            {
+                app.Tap(c => c.Id("nextView"));
+            }
+
+            var firstElements = app.Query(c => c.Id("name")).Select(x => x.Text).ToList();
+            app.ScrollDown();
+            var secondElements = app.Query(c => c.Id("name")).Select(x => x.Text).ToList();
+
+
+            foreach (var s in secondElements)
+            {
+                firstElements.Add(s);
+            }
+
+            var elements = firstElements.Distinct().ToArray();
+            string[] expectedElements =
+            {
+                "Электроника",
+                "Компьютеры и сети",
+                "Бытовая техника",
+                "Стройка и ремонт",
+                "Дом и сад",
+                "Авто и мото",
+                "Красота и спорт",
+                "Детям и мамам",
+                "Работа и офис"
+            };
+
+            var expected = expectedElements.ToArray();
+
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                Console.WriteLine($"{elements[i]} = {expected[i]} : {elements[i] == expected[i]}");
+            }
+
+            Assert.AreEqual(9, elements.Count());
+            Assert.IsTrue(expected.SequenceEqual(elements));
         }
     }
 }
